baa3e817c6a620c1d8d6deca2131d597d5eed89f
Monkey <ole.orke@hotmail.com>
2018-08-06 13:42:40 -0700

    add backend boilerplate

--- server/index.js
+++ server/index.js
@@ -1,8 +1,47 @@
-import http from 'http';
+var app = require('express')();
+var server = require('http').Server(app);
+var path = require("path");
+var io = require('socket.io')(server);
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-console.log('Server running at http://127.0.0.1:1337/');
+// set passport middleware to first try local strategy
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+// session configuration
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// connect passport to express via express middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+io.on('connection', function (socket) {
+  socket.emit('msg', { hello: 'world' });
+  socket.on('cmd', function (data) {
+    console.log(data);
+  });
+});
+
+server.listen(8080);
--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import io from "socket.io-client";
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 export default class App extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = {editorState: EditorState.createEmpty()};
+  this.onChange = (editorState) => this.setState({editorState});
+}
+
+componentDidMount() {
+  const socket = io('http://localhost:8080');
+  socket.on('connect', function(){console.log('ws connect')});
+  socket.on('disconnect', function(){console.log('ws disconnect')});
+  socket.on('msg', function(data){
+    console.log('ws msg:', data);
+    socket.emit('cmd', {foo:123})
+  });
+}
+
+_onBoldClick(e) {
+  e.preventDefault()
+  this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+}
+
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
-      <Button color="primary">Bold</Button>
+      <h1>Welcome to this editor!</h1>
+      <Button onMouseDown={(e) => this._onBoldClick(e)}>BOLD</Button>
+      <Editor
+        editorState={this.state.editorState}
+        onChange={this.onChange}
+      />
     </div>);
   }
 }
