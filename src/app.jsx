7079d68784eaa9fbc27ee69aba04dfbf8778da3b
Monkey <ole.orke@hotmail.com>
2018-08-06 14:29:48 -0700

    added inline styles and color

--- src/app.jsx
+++ src/app.jsx
@@ -1,38 +1,85 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import io from "socket.io-client";
-import { Editor, EditorState, RichUtils } from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+import { GithubPicker } from 'react-color';
 
 export default class App extends React.Component {
   constructor(props) {
-  super(props);
-  this.state = {editorState: EditorState.createEmpty()};
-  this.onChange = (editorState) => this.setState({editorState});
-}
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      showColorPicker: false,
+      editorColor: "black"
+    };
+    this.onChange = (editorState) => this.setState({editorState});
+  }
 
-componentDidMount() {
-  const socket = io('http://localhost:8080');
-  socket.on('connect', function(){console.log('ws connect')});
-  socket.on('disconnect', function(){console.log('ws disconnect')});
-  socket.on('msg', function(data){
-    console.log('ws msg:', data);
-    socket.emit('cmd', {foo:123})
-  });
-}
+  componentDidMount() {
+    const socket = io('http://localhost:8080');
+    socket.on('connect', function() {
+      console.log('ws connect')
+    });
+    socket.on('disconnect', function() {
+      console.log('ws disconnect')
+    });
+    socket.on('msg', function(data) {
+      console.log('ws msg:', data);
+      socket.emit('cmd', {foo: 123})
+    });
+  }
 
-_onBoldClick(e) {
-  e.preventDefault()
-  this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-}
+  _onBoldClick(e) {
+    e.preventDefault()
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  _onItalicClick(e){
+    e.preventDefault();
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+
+  _onUnderlineClick(e){
+    e.preventDefault();
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+
+  _onColorClick(e){
+    e.preventDefault();
+    this.setState({
+      showColorPicker: !this.state.showColorPicker
+    });
+  }
+
+  handleColorChange(color, e) {
+    e.preventDefault();
+    console.log(color);
+    this.setState({
+      editorColor: color.hex,
+      showColorPicker: false
+    });
+  }
 
   render() {
+    const editorStyle = {
+      backgroundColor: "#f2f2f2",
+      minHeight: "100vh",
+      padding: "10px",
+      margin: "20px",
+      color: this.state.editorColor
+    }
     return (<div>
       <h1>Welcome to this editor!</h1>
-      <Button onMouseDown={(e) => this._onBoldClick(e)}>BOLD</Button>
-      <Editor
-        editorState={this.state.editorState}
-        onChange={this.onChange}
-      />
+      <Button style={{fontWeight: "bold"}} onMouseDown={(e) => this._onBoldClick(e)}>BOLD</Button>
+      <Button style={{fontStyle: "italic"}} onMouseDown={(e) => this._onItalicClick(e)}>ITALIC</Button>
+      <Button style={{textDecoration: "underline"}} onMouseDown={(e) => this._onUnderlineClick(e)}>UNDERLINE</Button>
+      <Button style={{textDecoration: "underline"}} onMouseDown={(e) => this._onColorClick(e)}>COLOR</Button>
+      <div>
+        {this.state.showColorPicker ? <div style={{position: "absolute", left: "275px"}}><GithubPicker onChange={(color, e) => this.handleColorChange(color, e)}/></div> : <div></div>}
+      </div>
+      <div style={editorStyle}>
+        <Editor editorState={this.state.editorState} onChange={this.onChange}/>
+      </div>
     </div>);
   }
 }
